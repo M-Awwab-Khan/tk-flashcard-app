e082ef6043c564c812f44d2a2b671c1fde2782e6
Awwab <mawwabkhank2006@gmail.com>
2024-02-06 15:08:39 +0500

    basic window and canvas setup

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
 BACKGROUND_COLOR = "#B1DDC6"
+from tkinter import *
+
+
+window = Tk()
+window.title('Flashcard Application')
+window.config(bg='#B1DDC6', padx=50, pady=50)
+
+card = Canvas(width=800, height=526)
+card.grid(row=0, column=0, columnspan=2)
+
+
+window.mainloop()
