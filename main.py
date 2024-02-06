bcabadafe5ecb7a65b5c66180c0a24ff10db524a
Awwab <mawwabkhank2006@gmail.com>
2024-02-06 15:17:09 +0500

    basic gui setup with card image and buttons image

--- main.py
+++ main.py
@@ -6,8 +6,18 @@ window = Tk()
 window.title('Flashcard Application')
 window.config(bg='#B1DDC6', padx=50, pady=50)
 
-card = Canvas(width=800, height=526)
-card.grid(row=0, column=0, columnspan=2)
+canvas = Canvas(width=800, height=526, bg='#B1DDC6', highlightthickness=0)
+card_img = PhotoImage(file='./images/card_front.png')
+canvas.create_image(400, 263, image=card_img)
 
+canvas.grid(row=0, column=0, columnspan=2)
+
+correct_img = PhotoImage(file="./images/right.png")
+button = Button(image=correct_img, highlightthickness=0)
+button.grid(row=1, column=0)
+
+wrong_img = PhotoImage(file="./images/wrong.png")
+button = Button(image=wrong_img, highlightthickness=0)
+button.grid(row=1, column=1)
 
 window.mainloop()
